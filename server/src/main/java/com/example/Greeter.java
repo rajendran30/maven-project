38e5402162d9b618baf2ad06c3b959b6685f4be0
Rajendran Rathinasabapathy <rajendran30@gmail.com>
2017-07-30 18:57:00 -0700

    made the greet function final

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,7 +17,7 @@ public class Greeter {
 *@return greeting string
 */
 
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
